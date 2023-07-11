4d69f66011685a5df8346518dbe795465a9f7d69
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-11 21:23:34 +0900

    yuweet 가져오는 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const [yuweet, setYuweet] = useState("");
+  const [yuweets, setYuweets] = useState([]);
+
+  const getYuweets = async () => {
+    const dbYuweets = await getDocs(collection(dbService, "yuweets"));
+    dbYuweets.forEach((document) => {
+      const yuweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setYuweets((prev) => [yuweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getYuweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -21,12 +37,20 @@ const Home = () => {
     setYuweet(value);
   };
 
+  console.log(yuweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={yuweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Yuweet" />
       </form>
+      <div>
+        {yuweets.map((yw) => (
+          <div key={yw.id}>
+            <h4>{yw.yuweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
